2e91d15045cced5322ff3f71ce3332af997ba600
EvanMacIsaac <macisaace@limestone.on.ca>
2022-06-02 09:27:12 -0400

    Gravity
    applying gravity and making it so you can't go threw the Platform

--- Summative-3/PlayerOne.java
+++ Summative-3/PlayerOne.java
@@ -10,6 +10,10 @@ public class PlayerOne extends Actor
 {
     int deltaX = 0;
     int deltaY = 0;
+    // movement speed.
+    final int SPEED_X = 7;
+    int gravity = 1;
+    int jumpHight = -15;
     boolean isInAir = true;
     /**
      * Act - do whatever the PlayerOne wants to do. This method is called whenever
@@ -18,23 +22,28 @@ public class PlayerOne extends Actor
     public void act() 
     {
         // Add your action code here.
+        bumpHead();
+        movementControls();
+    }    
+    public void movementControls()
+    {
         deltaX = 0;
         if(Greenfoot.isKeyDown("left"))
         {
-            deltaX = deltaX - 5;
+            deltaX = deltaX - SPEED_X;
         }
         if(Greenfoot.isKeyDown("right"))
         {
-            deltaX = deltaX + 5;
+            deltaX = deltaX + SPEED_X;
         }
         if (Greenfoot.isKeyDown("space")&& isInAir == false)
         {
-            deltaY = -15;
+            deltaY = jumpHight;
             isInAir = true;
         }
         // update position
         setLocation(getX() + deltaX, getY() + deltaY);
-        
+        applyGravity();
         // check weather falling.
         if(isTouching(Brick.class))
         {
@@ -48,5 +57,61 @@ public class PlayerOne extends Actor
             deltaY = deltaY + 1;
             isInAir = true;
         }
-    }    
+    }
+    /**
+     *  if on block able to jump. if not you are not apply gravity.
+     */
+    public void applyGravity()
+    {
+        int height = getImage().getHeight();
+        // checking whether falling.
+        Actor platform = getOneObjectAtOffset(0, height / 2, Brick.class);
+        if(platform != null)
+        {
+            //standing on brick don't apply gravity.
+            deltaY = 0;
+            isInAir = false;
+            moveOnTopOfObject(platform);
+        }
+        else
+        {
+            //not touching the brick apply gravity.
+            deltaY = deltaY + gravity;
+            isInAir = true;
+        }
+    }
+    /**
+     *  if ObjectAtOffset collides inact stayUnderBrick.
+     */
+    public void bumpHead()
+    {
+        int height = getImage().getHeight();
+        Actor bump = getOneObjectAtOffset(0, - height / 2, Brick.class);
+        if (bump != null)
+        {
+            stayUnderBrick(bump);
+        }
+    }
+    /**
+     *  stay under the brick when collision occurs.
+     */
+    public void stayUnderBrick(Actor object)
+    {
+         int height = getImage().getHeight();
+         int objectHeight = object.getImage().getHeight();
+        
+         setLocation(getX(), object.getY() + objectHeight / 2 +  height / 2 + 1);
+    }
+    /**
+     *  stay on top of the Brick when standing on it.
+     */
+    public void moveOnTopOfObject(Actor object)
+    {
+        int height = getImage().getHeight();
+        int objectHeight = object.getImage().getHeight();
+        
+        // Adjust character position so that its bottom edge is just touching top edge of object.
+        setLocation(getX(), object.getY() - objectHeight / 2 - height / 2 + 1); // Plus 1 to make character sink into object by 1 pixel.
+        
+    }
 }
